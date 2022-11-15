78e82b3a9619d46eb0400928f8cb92fc88c88662
juan.garcia <juan.garcia70@uabc.edu.mx>
2022-11-14 20:39:20 -0800

    Agregando Usuario.java

---
+++ src/example/Usuario.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package example;
+
+/**
+ *
+ * @author juang
+ */
+public class Usuario {
+    
+    private int id;
+    private String nombreUsuario;
+    private String contraseña;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "id=" + id + ", nombreUsuario=" + nombreUsuario + ", contrase\u00f1a=" + contraseña + '}';
+    }
+   
+}
